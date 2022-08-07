4dbbb63b1a881019ae46501b965fe2f5f987b7cc
Jordan Gouw <jordan.gouw2001@gmail.com>
2022-08-07 19:26:46 +0700

    Navbar commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+  height: 60px;
+  background: red;
+`
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <Nav>
+      <h1>Navbar</h1> 
+    </Nav>
   )
 }
 
